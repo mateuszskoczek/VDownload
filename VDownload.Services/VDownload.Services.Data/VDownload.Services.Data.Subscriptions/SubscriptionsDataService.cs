032c08a810f324d93719d1a199bc755db86b5919
Mateusz Skoczek <mateusz5432160@gmail.com>
2024-03-08 00:59:34 +0100

    subscription data service added

---
+++ VDownload.Services/VDownload.Services.Data/VDownload.Services.Data.Subscriptions/SubscriptionsDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VDownload.Services.Data.Subscriptions
+{
+    public class SubscriptionsDataService
+    {
+    }
+}
